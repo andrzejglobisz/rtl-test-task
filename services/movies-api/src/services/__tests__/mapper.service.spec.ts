5bdf846bea1499592c807f95efe0c136cefbd427
Andrzej Globisz <Andrzej_Globisz@epam.com>
2018-09-18 09:23:17 +0200

    removed mapper for DB date - moved to mongo query

--- services/movies-api/src/services/__tests__/mapper.service.spec.ts
+++
@@ -1,8 +0,0 @@
-import { mapToMovie } from '../mapper.service';
-import { moviesFromDbMock, moviesMock } from '../../__mocks__/movies.mock';
-
-describe('Mapper service ', () => {
-    it('should map to Movies and Cast interface', () => {
-        expect(mapToMovie(moviesFromDbMock)).toEqual(moviesMock);
-    });
-});
--- services/movies-api/src/services/movies.service.ts
+++ services/movies-api/src/services/movies.service.ts
@@ -1,16 +1,18 @@
 import { Movie } from '../types/types';
 
 import MovieModel, { MovieModel as MovieModelType } from '../models/Movie';
-import { mapToMovie } from './mapper.service';
 import CONFIG from '../config';
 
 export class MoviesService {
     public getMovies = async (pageNo: number): Promise<Movie[]> => {
-        const moviesFromDb: MovieModelType[] = await MovieModel.find({
-            id: { $gte: this.calcFirstMovieId(pageNo), $lt: this.calcLastMovieId(pageNo) },
-        }).exec();
+        const moviesFromDb: MovieModelType[] = await MovieModel.find(
+            {
+                id: { $gte: this.calcFirstMovieId(pageNo), $lt: this.calcLastMovieId(pageNo) },
+            },
+            { id: 1, _id: 0, name: 1, 'cast.id': 1, 'cast.name': 1, 'cast.birthday': 1 }
+        ).exec();
 
-        return mapToMovie(moviesFromDb);
+        return moviesFromDb;
     };
 
     private calcFirstMovieId = (pageNo: number) => pageNo * CONFIG.MOVIES_PER_PAGE;
