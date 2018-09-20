293b6aced81e08a0612468b993fec4217bf497bb
Andrzej Globisz <Andrzej_Globisz@epam.com>
2018-09-20 15:03:09 +0200

    using skip and limit in mongo search query

--- services/movies-api/src/services/movies.service.ts
+++ services/movies-api/src/services/movies.service.ts
@@ -6,16 +6,11 @@ import CONFIG from '../config';
 export class MoviesService {
     public getMovies = async (pageNo: number): Promise<Movie[]> => {
         const moviesFromDb: MovieModelType[] = await MovieModel.find(
-            {
-                id: { $gte: this.calcFirstMovieId(pageNo), $lt: this.calcLastMovieId(pageNo) },
-            },
-            { id: 1, _id: 0, name: 1, 'cast.id': 1, 'cast.name': 1, 'cast.birthday': 1 }
+            {},
+            { id: 1, _id: 0, name: 1, 'cast.id': 1, 'cast.name': 1, 'cast.birthday': 1 },
+            { skip: CONFIG.MOVIES_PER_PAGE * pageNo, limit: CONFIG.MOVIES_PER_PAGE }
         ).exec();
 
         return moviesFromDb;
     };
-
-    private calcFirstMovieId = (pageNo: number) => pageNo * CONFIG.MOVIES_PER_PAGE;
-
-    private calcLastMovieId = (pageNo: number) => (pageNo + 1) * CONFIG.MOVIES_PER_PAGE;
 }
